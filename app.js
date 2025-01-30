6478cf110782e3b10a12280fb724e69a3bd9f3c5
Joao Vitor <soaresjvs@hotmail.com>
2025-01-30 14:54:50 -0300

    adicionar amigos na lista

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+    let campoAmigo = document.getElementById("amigo");
+
+    if(campoAmigo.value == ""){
+        alert("Por favor, insira um nome!");
+    }
+
+    amigos.push(campoAmigo.value);
+    console.log(amigos);
+    
+    campoAmigo.value = "";
+   
+}
